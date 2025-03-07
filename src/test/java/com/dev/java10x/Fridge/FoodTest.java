5fd96c1e641b523c4175291e571625ab211e4aca
jonatas <jonatas_dev@hotmail.com>
2025-03-07 18:42:18 -0300

    Implementando teste no código

---
+++ src/test/java/com/dev/java10x/Fridge/FoodTest.java
@@ -0,0 +1,14 @@
+package com.dev.java10x.Fridge;
+
+
+import com.dev.java10x.Fridge.Model.Food;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FoodTest {
+    @Test
+    public void testeFalso() {
+        assertTrue(true);
+    }
+}
