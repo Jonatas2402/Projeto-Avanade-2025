e25be9eb08c740e3b8b04e267e8ed268edd20c0c
jonatas <jonatas_dev@hotmail.com>
2025-03-27 12:04:45 -0300

    Refactore

--- src/main/java/com/dev/java10x/Fridge/Repository/FoodRepository.java
+++ src/main/java/com/dev/java10x/Fridge/Repository/FoodRepository.java
@@ -2,10 +2,12 @@ package com.dev.java10x.Fridge.Repository;
 
 import com.dev.java10x.Fridge.Model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //Essa classe é um repositório que será injetado no controlador.
 //O repositório é a camada de acesso a dados, onde terá todas as operações de CRUD.
 //Aqui é onde terá todas as operações de banco de dados.
+@Repository //Anotação que diz que essa classe é um repositório do Spring Boot.
 public interface FoodRepository extends JpaRepository<Food,Long> {
     //Os parametros genericos são o tipo da entidade e o tipo da chave primária.
     //Ela será uma interface que irá extender a interface JpaRepository.
